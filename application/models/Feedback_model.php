a8a4f6f182aa408d8ed26fefdfd64ccf179c1865
Shashank <shashankpallerla@gmail.com>
2018-11-26 20:34:54 -0600

    Git case sensitive ignore set false

---
+++ application/models/Feedback_model.php
@@ -0,0 +1,17 @@
+<?php
+
+class Feedback_model extends MY_Model
+{
+    public $table = 'feedback';
+    public $primary_key = 'id';
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->timestamps = FALSE;
+
+    }
+}
+
+
+?>
