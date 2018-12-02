e3724b03a791cd7ccb0a3a26dd25a01bdac79eae
Shashank <shashankpallerla@gmail.com>
2018-12-01 21:01:49 -0600

    Home page change

--- application/views/home.php
+++ application/views/home.php
@@ -3,13 +3,13 @@
 
     <br>
 
- <!--   <div class="jumbotron">
+    <div class="jumbotron">
         <div class="container">
-            <h1 class="display-3">Hello, world!</h1>
-            <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-            <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+            <h1 class="display-3">TravelWire</h1>
+            <p>A travel website</p>
+<!--            <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>-->
         </div>
-    </div>-->
+    </div>
 
     <div class="container">
         <div class="row">
